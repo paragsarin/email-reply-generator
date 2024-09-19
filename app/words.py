f171c53dafb7ed569726c19736e04a9d02ef6d36
paragsarin <42388306+paragsarin@users.noreply.github.com>
2024-09-19 15:53:04 -0400

    Update words.py

--- app/words.py
+++ app/words.py
@@ -10,12 +10,12 @@ class Words:
         self.chroma_client = chromadb.PersistentClient('vectorstore')
         self.collection = self.chroma_client.get_or_create_collection(name="concerns")
 
-    def load_portfolio(self):
+    def load_concerns(self):
         if not self.collection.count():
             for _, row in self.data.iterrows():
                 self.collection.add(documents=row["CatchWords"],
                                     metadatas={"links": row["Links"]},
                                     ids=[str(uuid.uuid4())])
 
-    def query_links(self, skills):
-        return self.collection.query(query_texts=skills, n_results=2).get('metadatas', [])
+    def query_links(self, concerns):
+        return self.collection.query(query_texts=concerns, n_results=2).get('metadatas', [])
